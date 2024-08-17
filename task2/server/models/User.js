5388142fe114d09ab0a938e61f7e150fd80f9998
saimani1232 <159790449+saimani1232@users.noreply.github.com>
2024-08-17 21:50:54 +0530

    Create User.js

---
+++ task2/server/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
